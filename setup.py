fab5415b4ad6ce29b558f668914c4605038ade42
Ankur Goswami <ankur.goswami@keysight.com>
2018-06-10 23:14:47 -0700

    Setting up to upload this to PyPI - Add some stuff to setup.py - Update Gitignore

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+setup(
+    name='seedlib',
+    packages=['seedlib'],
+    version='0.1',
+    description='An aggregation of random miscellaneous functions.',
+    author='Ankur Goswami',
+    author_email='ankur.goswami12@gmail.com',
+    url='https://github.com/ankur-gos/SeedLib',
+    classifiers=[],
+    install_requires=[
+        'pandas',
+    ],
+)
