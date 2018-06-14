c42414305322bda417f9be381a249724fc96c47e
Ankur Goswami <ankur.goswami@keysight.com>
2018-06-14 12:24:21 -0700

    Add Parzen Window functions

--- seedlib/main.py
+++ seedlib/main.py
@@ -4,6 +4,8 @@
 """
 
 import pandas as pd
+from math import sqrt, e, pi
+from numpy import linalg
 
 
 def cat(filename):
@@ -44,3 +46,19 @@ def get_n_sets(df, n=10, train_size=0.8):
         train_sets.append(train)
         test_sets.append(test)
     return zip(train_sets, test_sets)
+
+
+def gaussian_kernel(sigma, X, sample_X):
+    return e ** (-linalg.norm(X - sample_X) ** 2 / (2 * sigma ** 2)) / (sqrt(2 * pi * sigma ** 2))
+
+
+def parzen_window_gaussian(sigma, X, sample_X):
+    n = len(sample_X)
+    results = []
+    for x in X:
+        sum = 0
+        for sample_x in sample_X:
+            sum += gaussian_kernel(sigma, x, sample_x)
+        results.append(sum)
+    return [result / n for result in results]
+
